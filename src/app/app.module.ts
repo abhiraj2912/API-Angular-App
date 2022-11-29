ea678579f9296798a8cd25ad36f3b9f9d9fbdca8
abhiraj2912 <abhirajaravind408@gmail.com>
2022-11-29 10:22:27 +0530

    user info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,17 +6,41 @@ import { AppComponent } from './app.component';
 import { PassengerDataComponent } from './passenger-data/passenger-data.component';
 import { PublicDataComponent } from './public-data/public-data.component';
 import { PublicAPIComponent } from './public-api/public-api.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myroute:Routes=[
+  {
+    path:"",
+    component:PassengerDataComponent
+  },
+  {
+    path:"api",
+    component:PublicAPIComponent
+  },
+  {
+    path:"publicdata",
+    component:PublicDataComponent
+  },
+  {
+    path:"userinfo",
+    component:UserInfoComponent
+  }
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     PassengerDataComponent,
     PublicDataComponent,
-    PublicAPIComponent
+    PublicAPIComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
